fb0cf244c50758cfd5d6ede153a8beb5ad4b3bf9
edvinsternvik <e.sternvik@gmail.com>
2024-07-23 16:21:57 +0200

    Add types for representing values within some range

--- src/misc.rs
+++ src/misc.rs
@@ -1,5 +1,79 @@
 use core::slice::{Iter, IterMut};
 
+pub struct DSignal {
+    value: i32,
+    min: i32,
+    max: i32,
+}
+
+impl<const MIN: i32, const MAX: i32> From<Signal<MIN, MAX>> for DSignal {
+    fn from(value: Signal<MIN, MAX>) -> Self {
+        DSignal::new(value.value(), MIN, MAX)
+    }
+}
+
+impl DSignal {
+    pub fn new(value: i32, min: i32, max: i32) -> Self {
+        Self {
+            value: i32::clamp(value, min, max),
+            min,
+            max,
+        }
+    }
+
+    pub fn convert(self, min: i32, max: i32) -> DSignal {
+        let value = (self.value - self.min) as u64;
+        let scaled = (value * (max - min) as u64) / (self.max - self.min) as u64;
+        let value2 = (scaled as i64 + min as i64) as i32;
+
+        Self::new(value2, min, max)
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+
+    pub fn min(&self) -> i32 {
+        self.min
+    }
+
+    pub fn max(&self) -> i32 {
+        self.max
+    }
+}
+
+pub struct Signal<const MIN: i32, const MAX: i32>(i32);
+
+impl<const MIN: i32, const MAX: i32> From<DSignal> for Signal<MIN, MAX> {
+    fn from(value: DSignal) -> Self {
+        Self(value.convert(MIN, MAX).value())
+    }
+}
+
+impl<const MIN: i32, const MAX: i32> From<i32> for Signal<MIN, MAX> {
+    fn from(value: i32) -> Self {
+        Signal(i32::clamp(value, MIN, MAX))
+    }
+}
+
+impl<const MIN: i32, const MAX: i32> Signal<MIN, MAX> {
+    pub fn new(value: i32, min: i32, max: i32) -> Self {
+        DSignal::new(value, min, max).into()
+    }
+
+    pub fn convert<const MIN2: i32, const MAX2: i32>(self) -> Signal<MIN2, MAX2> {
+        let value = (self.value() - MIN) as u64;
+        let scaled = (value * (MAX2 - MIN2) as u64) / (MAX - MIN) as u64;
+        let value2 = (scaled as i64 + MIN2 as i64) as i32;
+
+        value2.into()
+    }
+
+    pub fn value(&self) -> i32 {
+        self.0
+    }
+}
+
 pub struct FixedSet<T, const N: usize> {
     array: [T; N],
     n: usize,
